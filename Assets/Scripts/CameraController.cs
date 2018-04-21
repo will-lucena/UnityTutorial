8e4ca30593609357a546249d576311592856d1e5
Will Lucena <Will>
2018-04-21 18:50:05 -0300

    Camera script finalizado, sprites exportados com pacote

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform targetTransform;
     public Transform[] borders;
+    public Transform[] playerBorders;
+
+    public float xOffset;
+    public float yOffset;
 
     // Update is called once per frame
 	void Update ()
@@ -13,38 +15,38 @@ public class CameraController : MonoBehaviour
         //*
         float xPos = checkWidth();
         float yPos = checkHeigth();
-        
+
         transform.position = new Vector3(xPos, yPos, -10);
         /**/
     }
 
     private float checkHeigth()
     {
-        if (transform.position.y < borders[3].position.y)
+        if (playerBorders[3].position.y < borders[3].position.y)
         {
-            return borders[2].position.y;
+            return borders[3].position.y + 5;
         }
-        if (transform.position.y > borders[1].position.y)
+        if (playerBorders[1].position.y > borders[1].position.y)
         {
-            return borders[1].position.y;
+            return borders[1].position.y - 5;
         }
 
-        return targetTransform.position.y;
+        return targetTransform.position.y + yOffset;
     }
 
     private float checkWidth()
     {
-        if (transform.position.x < borders[0].position.x - 1)
+        if (Mathf.Min(playerBorders[0].position.x, playerBorders[2].position.x) + xOffset < borders[0].position.x)
         {
-            return borders[0].position.x;
+            return borders[0].position.x + 9;
         }
 
-        if (transform.position.x > borders[2].position.x + 1)
+        if (Mathf.Max(playerBorders[0].position.x, playerBorders[2].position.x) + xOffset > borders[2].position.x)
         {
-            return borders[2].position.x;
+            return borders[2].position.x - 9;
         }
 
-        return targetTransform.position.x;
+        return targetTransform.position.x + xOffset;
     }
     
 }
