824bf9a9f323156725ad31abb2ee631b51413f24
Will <williamaster1@gmail.com>
2018-04-30 16:43:43 -0300

    traduzindo codigo para portugues

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform alvoTransform;
+    public Transform[] bordas;
+    public Transform[] bordasJogador;
+
+    public float sobraX;
+    public float sobraY;
+
+    // Update is called once per frame
+	void Update ()
+    {
+        //*
+        float posicaoX = checarPosicao();
+        float posicaoY = checarAltura();
+
+        transform.position = new Vector3(posicaoX, posicaoY, -10);
+        /**/
+    }
+
+    private float checarAltura()
+    {
+        if (bordasJogador[3].position.y < bordas[3].position.y)
+        {
+            return bordas[3].position.y + 5;
+        }
+        if (bordasJogador[1].position.y > bordas[1].position.y)
+        {
+            return bordas[1].position.y - 5;
+        }
+
+        return alvoTransform.position.y + sobraY;
+    }
+
+    private float checarPosicao()
+    {
+        if (Mathf.Min(bordasJogador[0].position.x, bordasJogador[2].position.x) + sobraX < bordas[0].position.x)
+        {
+            return bordas[0].position.x + 9;
+        }
+
+        if (Mathf.Max(bordasJogador[0].position.x, bordasJogador[2].position.x) + sobraX > bordas[2].position.x)
+        {
+            return bordas[2].position.x - 9;
+        }
+
+        return alvoTransform.position.x + sobraX;
+    }
+    
+}
---
+++ Assets/Scripts/MovimentacaoJogador.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class MovimentacaoJogador : MonoBehaviour
+{
+    private Rigidbody2D corpo;
+    private Animator controladorAnimacao;
+    private float entradaHorizontal;
+    private bool movendoPraFrente;
+
+    public float modificadorVelocidade;
+    public float forcaPulo;
+
+	// Use this for initialization
+	void Start ()
+    {
+        corpo = GetComponent<Rigidbody2D>();
+        controladorAnimacao = GetComponent<Animator>();
+        movendoPraFrente = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        entradaHorizontal = Input.GetAxis("Horizontal");
+
+        if ((entradaHorizontal < 0 && movendoPraFrente) || (entradaHorizontal > 0 && !movendoPraFrente))
+        {
+            girar();
+        }
+        corpo.velocity = new Vector3(modificadorVelocidade * entradaHorizontal, corpo.velocity.y, 0);
+        controladorAnimacao.SetFloat("Velocidade", Mathf.Abs(entradaHorizontal));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controladorAnimacao.SetTrigger("Pular");
+            corpo.AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
+        }
+	}
+    
+    private void girar()
+    {
+        if (movendoPraFrente)
+        {
+            transform.rotation = Quaternion.Euler(0, 190, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        movendoPraFrente = !movendoPraFrente;
+    }
+}
